3c84c6c11c52f8c17619287845d63a947c1baf65
Andrew Casey <andrew.casey@microsoft.com>
2023-04-21 11:08:06 -0700

    Fix build

--- src/Middleware/HttpLogging/samples/HttpLogging.Sample/Controllers/CodeFile.cs
+++ src/Middleware/HttpLogging/samples/HttpLogging.Sample/Controllers/CodeFile.cs
@@ -5,40 +5,39 @@ using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace WebApplication1.Controllers
+namespace WebApplication1.Controllers;
+
+public class EchoPayload
 {
-    public class EchoPayload
+    public string Message { get; set; } = string.Empty;
+    public string Details { get; set; } = string.Empty;
+}
+
+[Route("echo")]
+[ApiController]
+public class EchoController : ControllerBase
+{
+    ILogger<EchoController> _logger;
+
+    public EchoController(ILogger<EchoController> logger)
     {
-        public string Message { get; set; } = string.Empty;
-        public string Details { get; set; } = string.Empty;
+        _logger = logger;
+        _logger.LogInformation($"Constructor called");
     }
 
-    [Route("echo")]
-    [ApiController]
-    public class EchoController : ControllerBase
+    // POST api/<TestController>
+    [HttpGet]
+    public string Get()
     {
-        ILogger<EchoController> _logger;
+        _logger.LogInformation($"Get called");
+        return $"Hello from {this.GetType().FullName}.  Send me a post.";
+    }
 
-        public EchoController(ILogger<EchoController> logger)
-        {
-            _logger = logger;
-            _logger.LogInformation($"Constructor called");
-        }
-
-        // POST api/<TestController>
-        [HttpGet]
-        public string Get()
-        {
-            _logger.LogInformation($"Get called");
-            return $"Hello from {this.GetType().FullName}.  Send me a post.";
-        }
-
-        // POST api/<TestController>
-        [HttpPost]
-        public EchoPayload Post([FromBody] EchoPayload thePayload)
-        {
-            _logger.LogInformation($"Post called");
-            return thePayload;
-        }
+    // POST api/<TestController>
+    [HttpPost]
+    public EchoPayload Post([FromBody] EchoPayload thePayload)
+    {
+        _logger.LogInformation($"Post called");
+        return thePayload;
     }
 }
--- src/Middleware/HttpLogging/samples/HttpLogging.Sample/Program.cs
+++ src/Middleware/HttpLogging/samples/HttpLogging.Sample/Program.cs
@@ -6,52 +6,50 @@ using System.Net.Sockets;
 using System.Text;
 using System.Text.Json;
 
-namespace WebApplication1
+namespace WebApplication1;
+public class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
-            builder.Services.AddControllers();
-            builder.Services.AddHttpLogging(options => {
-                options.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.All;
-            });
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddControllers();
+        builder.Services.AddHttpLogging(options => {
+            options.LoggingFields = Microsoft.AspNetCore.HttpLogging.HttpLoggingFields.All;
+        });
 
-            var app = builder.Build();
+        var app = builder.Build();
 
-            //
-            //  Uncomment the following lines to catch the decryption error and return a 400 response to the client.
-            //
-            //app.Use(async (context, next) =>
-            //{
-            //    try
-            //    {
-            //        await next(context);
-            //    }
-            //    catch (IOException x)
-            //    {
-            //        if ((x.InnerException is System.ComponentModel.Win32Exception wx) && (wx.NativeErrorCode == unchecked((int)0x80090330)))
-            //        {
-            //                app.Logger.LogWarning($"Client encryption error detected!  Returning 400 to caller.");
-            //
-            //                context.Response.StatusCode = StatusCodes.Status400BadRequest;
-            //                context.Response.ContentType = "application/json";
-            //                await context.Response.WriteAsync(JsonSerializer.Serialize(new { Code = $"TlsDecryption", Message = $"Could not decrypt client transmitted data" }));
-            //        }
-            //        else
-            //        {
-            //            throw;
-            //        }
-            //    }
-            //});
+        //
+        //  Uncomment the following lines to catch the decryption error and return a 400 response to the client.
+        //
+        //app.Use(async (context, next) =>
+        //{
+        //    try
+        //    {
+        //        await next(context);
+        //    }
+        //    catch (IOException x)
+        //    {
+        //        if ((x.InnerException is System.ComponentModel.Win32Exception wx) && (wx.NativeErrorCode == unchecked((int)0x80090330)))
+        //        {
+        //                app.Logger.LogWarning($"Client encryption error detected!  Returning 400 to caller.");
+        //
+        //                context.Response.StatusCode = StatusCodes.Status400BadRequest;
+        //                context.Response.ContentType = "application/json";
+        //                await context.Response.WriteAsync(JsonSerializer.Serialize(new { Code = $"TlsDecryption", Message = $"Could not decrypt client transmitted data" }));
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+        //});
 
-            //app.UseHttpsRedirection();
-            app.UseHttpLogging();
-            app.UseAuthorization();
-            app.MapControllers();
+        //app.UseHttpsRedirection();
+        app.UseHttpLogging();
+        app.UseAuthorization();
+        app.MapControllers();
 
-            app.Run();
-        }
+        app.Run();
     }
 }
